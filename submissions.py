8b62d058819566018aa8c5dbb1c321d79fb9de29
called2voyage <thesboys@gmail.com>
2020-09-03 09:23:05 -0400

    Add search_title

--- submissions.py
+++ submissions.py
@@ -1,5 +1,14 @@
+import re
+
 def loop_submissions(subreddit, func, limit, *args):
      results = []
      for submission in subreddit.new(limit=limit):
          results.append(func(submission, *args))
      return results
+
+def search_title(submission, *queries):
+    found = False
+    for query in queries:
+        if re.search(query, submission.title, re.IGNORECASE):
+            found = True
+    return found
